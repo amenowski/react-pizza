afdced20b7c66576da794377be8a81d42734c742
contactserus@gmail.com <[contact-serus@gmail.com]>
2023-10-19 18:15:24 +0200

    first commit

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -36,7 +36,7 @@ function Order() {
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
   return (
-    <div className="space-y-8 px-4 py-6">
+    <div className="space-y-8 px-4 py-7">
       <div className="flex flex-wrap items-center justify-between gap-2">
         <h2 className="text-xl font-semibold">Order #{id} status</h2>
 
